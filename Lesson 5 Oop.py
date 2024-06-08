20fe87899942ee6a489c1e1adf53c59647e482be
nino_kvinikadze <kvinikadze.n.99@gmail.com>
2024-06-08 17:08:48 +0400

    firstt commit

--- Lesson 5 Oop.py
+++ Lesson 5 Oop.py
@@ -1,4 +1,110 @@
 
+
+#f(x) = 2x+1     g(x)= 3-x    so   f(g(x))=2(3-x)+1=6-2x+1=7-2x
+
+# def f(z):
+#     def inner():
+#         return 2 * z + 1
+#     return inner
+#
+# def g(x):
+#     return 3 - x
+#
+# result = f(g(5))()
+# print(result)
+
+#
+# def f(z):
+#     return 2 * z + 1
+#
+#
+# def g(x):
+#     return 3 - x
+#
+# result = f(g(5))
+# print(result)
+
+
+
+
+
+# class Human:
+#     height = 170
+#     eyes_number = 2
+#
+# print(Human.height)
+#
+# Human.Head = 1 #დავამატოთ კლასს წევრი
+# print(Human.Head)
+
+
+class Human:
+    height = 170
+    eyes_number = 2  #ეს არის პარამეტრები
+
+    def __init__ (self, saxeli, asaki):
+        self.name = saxeli
+        self.age = asaki
+
+    def say_name(self):
+        print(f"hi {self.name}, you are, {self.age} years old")
+    @staticmethod #ამ მეთოდს ვიყენებთ მაშინ, როდესაც განსაზღვრულ ფუნქციას კლასის პარამეტრები ცვლადად არ სჭირდება
+    def sleeping():
+        print("Zzz")   #ეს არის მეთოდი
+
+    @classmethod #ეს მეთოდი გვჭირდება მაშინ, როდესაც ინფორმაცია კლასიდან უნდა წამოიღოს
+    def say_height(cls): #აქ მიღებულია რო ეს დავწეროთ. თავისი თავის სახელს გადასცემს
+        print(f"am saying {cls.height}")
+
+    def __repr__(self):
+        return f"Human({self.name}, {self.age})"
+
+
+Human.sleeping()
+Human.say_height()
+
+# human1 = Human()
+# print(human1)
+#
+# human1.name = "nino"
+# human1.say_name()
+
+human2 = Human("nini", 24)
+
+human2.say_name()
+print(human2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # class Human:
 #     weight = 60
 #     height = 170
@@ -40,80 +146,80 @@
 #     def sleeping():
 #         print("ZZZZZZzzzzzZZzzz...")
 
-
-class Cats(Animals):
-
-    def __init__(self, name, weight):
-        Cats.__init__(self, name)
-        self.weight = weight
-
-    @staticmethod
-    def drinking():
-        print("I love milk")
-
-cats(Animals) = cats('fiso', 24)
-print(cats(Animals))
-
-
-
-
-
-class Vehicle:
-    def __init__(self, brand, model):
-        self.brand = brand
-        self.model = model
-
-    @staticmethod
-    def move():
-        print("I am Moving...")
-
-
-class Car(Vehicle):
-    pass
-
-class Ship(Vehicle):
-    @staticmethod
-    def move():
-        print("I am Swimming...")
-
-
-class Airplane(Vehicle):
-    @staticmethod
-    def move():
-        print("I am Flying...")
-
-
-car = Car("BMW", "X5")
-ship = Ship("SomeCompany", "dasdasds")
-plane = Airplane("Boing", "sasdads")
-
-car.move()
-
-
-
-from abc import ABC, abstractmethod
-
-class Figures(ABC):
-
-    @abstractmethod
-    def area(self):
-        pass
-
-    @abstractmethod
-    def perimeter(self):
-        pass
-
-class Cycle(Figures):
-    def __init__(self, r):
-        self.radius = r
-
-    def area(self):
-        print(3.14 * self.radius * self.radius)
-
-    def area(self):
-        print(2 * 3.14 * self.radius)
-
-
-
-
-
+#
+# class Cats(Animals):
+#
+#     def __init__(self, name, weight):
+#         Cats.__init__(self, name)
+#         self.weight = weight
+#
+#     @staticmethod
+#     def drinking():
+#         print("I love milk")
+#
+# cats(Animals) = cats('fiso', 24)
+# print(cats(Animals))
+#
+#
+#
+#
+#
+# class Vehicle:
+#     def __init__(self, brand, model):
+#         self.brand = brand
+#         self.model = model
+#
+#     @staticmethod
+#     def move():
+#         print("I am Moving...")
+#
+#
+# class Car(Vehicle):
+#     pass
+#
+# class Ship(Vehicle):
+#     @staticmethod
+#     def move():
+#         print("I am Swimming...")
+#
+#
+# class Airplane(Vehicle):
+#     @staticmethod
+#     def move():
+#         print("I am Flying...")
+#
+#
+# car = Car("BMW", "X5")
+# ship = Ship("SomeCompany", "dasdasds")
+# plane = Airplane("Boing", "sasdads")
+#
+# car.move()
+#
+#
+#
+# from abc import ABC, abstractmethod
+#
+# class Figures(ABC):
+#
+#     @abstractmethod
+#     def area(self):
+#         pass
+#
+#     @abstractmethod
+#     def perimeter(self):
+#         pass
+#
+# class Cycle(Figures):
+#     def __init__(self, r):
+#         self.radius = r
+#
+#     def area(self):
+#         print(3.14 * self.radius * self.radius)
+#
+#     def area(self):
+#         print(2 * 3.14 * self.radius)
+#
+#
+#
+#
+#
--- NinoKvinikadze_HW5.py
+++ NinoKvinikadze_HW5.py
@@ -1,13 +1,66 @@
 # 1. შექმენი ტრანსპორტის კლასი მინიმუმ 4 კლასის პარამეტრით
-
 # 2. დაამატე ერთი სტატიკური მეთოდი.
-
 # 3. დაამატე ორი კლასის მეთოდი.
-
 # 4. დაამატე __init__ magic მეთოდი და მინიმუმ 3 ობიექტის პარამეტრი.
-
 # 5. დაამატე მინიმუმ 2, ობიექტის მეთოდი.
-
 # 6. დაამატე __repr__ magic მეთოდი.
-
 # 7. ზემოხსენებული კლასისგან შექმენი მინიმუმ 5 ობიექტი და გამოიძახე მისი ზოგიერთი მეთოდი და პარამეტრი.
+
+class Transport:
+    wheels = 5
+    SteeringWheel = 1
+    mirrors = 3
+    pedals = 2
+
+    def __init__ (self, brendi, weli, garbeni):
+        self.brand = brendi
+        self.year = weli
+        self.mile = garbeni
+
+    def say_brand(self):
+        print(f"The brand name is {self.brand}")
+
+    def say_mile(self):
+        print(f"ამ მანქანას აქვს {self.mile} მილი გარბენი")
+
+    @staticmethod
+    def moving():
+        print("შუქნიშანს დააკვირდი!")
+
+    @classmethod
+    def wheels_Number(cls):
+        print(f"ამ ტრანსპორტს აქვს {cls.wheels} ბორბალი")
+
+    @classmethod
+    def mirrors_Number(cls):
+        print(f"ამ ტრანსპორტს აქვს {cls.mirrors} სარკე")
+
+    def __repr__(self):
+        return f"Transport(brand: {self.brand}, year: {self.year}, mile: {self.mile})"
+
+
+car1=Transport('Toyota', 2015, 26000 )
+
+car1.moving()
+car1.wheels = 4
+car1.wheels_Number()
+car1.mirrors_Number()
+
+print(car1)
+
+car2 = Transport('Toyota', 2020, 38000 )
+car3 = Transport('Audi', 2017, 35000 )
+car4 = Transport('BMW', 2023, 30000 )
+car5 = Transport('Honda', 2020, 44000 )
+
+print(car2, car3, car4)
+
+car2.say_brand()
+car3.say_brand()
+car4.say_brand()
+car5.say_brand()
+
+car2.say_mile()
+car3.mirrors_Number()
+
+car5.moving()
